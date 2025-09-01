93a37e52dbdedcc9430233e6d451b3f097651363
sabin2000pro <sabinlungu293@gmail.com>
2025-09-01 14:47:23 +0100

    Added logout / resend e-mail verification endpoints

--- controllers/auth-controllers.ts
+++ controllers/auth-controllers.ts
@@ -21,24 +21,33 @@ export const logoutUserAsync = async (
   next: NextFunction
 ) => {};
 
+export const logoutAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+    return response.status(200).json({success: true, message: 'Logout User Here', user: {}, token: null})
+})
+
+export const forgotPasswordAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+    return response.status(200).json({success: true, message: 'Forgot Password Here', user: {}, token: null})
+
+})
+
+export const resetPasswordAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+    return response.status(200).json({success: true, message: 'Reset Password Endpoint' , user: {}, token: null})
+})
+
+export const getLoggedInUserAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+    return response.status(200).json({success: true, message: 'Get Logged In User Endpoint Here' , user: {}, token: null})
+})
+
 export const verifyEmailAddress = async (
-  request: Request,
-  response: Response,
-  next: NextFunction
-) => {};
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {};
 
-export const logoutAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction) => {
-
+export const resendEmailVerification = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+    return response.status(200).json({success: true, message: 'Resend e-mail verification here' , data: {} })
 })
 
-export const forgotPasswordAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction) => {
-
-})
-
-export const resetPasswordAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction) => {
-
-})
-
-export const getLoggedInUserAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction) => {
-
+export const updatePasswordAsync = expressAsyncHandler(async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+    return response.status(200).json({success: true, message: 'Update user password here' , data: {} })
 })
